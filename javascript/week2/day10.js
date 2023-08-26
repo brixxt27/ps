924d52f6afdd800f07f6d5f149e1c1f72bc0cba4
brixxt27 <brixxt27@gmail.com>
2023-08-26 23:56:44 +0900

    feat: 설계 실수로 처음부터 다시 풀어야 함
    역시 아니다 싶으면 아닌 것이였다. 현재 하나의 자료구조로 둘이 돌려 사용하는데 이렇게 하면 함수가 둘로 나뉘어질 수 밖에 없다.
    때문에 유지 보수도 굉장히 힘들다. 그래서 처음부터 다시 구현할 예정이다.
    class Area 에는 flag 하나만 존재할 예정이고, goorm 이 게임을 한 판하면, 그 flag 를 전부 지우고 다시 player가 할 예정이다.
    깊은 복사가 되지 않기 때문인데 JSON 방식도 사용해볼 예정이다.

--- javascript/week2/day10.js
+++ javascript/week2/day10.js
@@ -1,3 +1,14 @@
+/*
+N
+R C: player
+R C: goormi
+1U 2D ... N개 주어짐 
+3R 4L
+.
+.
+N개 주어짐
+*/
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -5,20 +16,21 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+class Area {
+  count;
+  command;
+  goormi = false;
+  player = false;
+};
+
 let N;
-let player = {};
 let goormi = {};
-let rawBoard = [];
+let player = {};
+let board = [];
 
 rl.on('line', (line) => {
   if (!N) {
     N = parseInt(line); // parseInt 는 string 메소드가 아니고, ES5에 내장 되어 있는 함수이다.
-  }
-  else if (Object.keys(player).length < 2) {
-    let [r, c] = line.split(' ')
-
-    player.r = parseInt(r);
-    player.c = parseInt(c);
   }
   else if (Object.keys(goormi).length < 2) {
     let [r, c] = line.split(' ')
@@ -26,11 +38,110 @@ rl.on('line', (line) => {
     goormi.r = parseInt(r);
     goormi.c = parseInt(c);
   }
-  else if (rawBoard.length < N) {
+  else if (Object.keys(player).length < 2) {
+    let [r, c] = line.split(' ')
 
+    player.r = parseInt(r);
+    player.c = parseInt(c);
+  }
+  else if (board.length < N) {
+    let arr = line.split(' ');
+    let line_arr = [];
+
+    arr.forEach(value => {
+      let input = new Area();
+
+      input.count = parseInt(value[0]);
+      input.command = value[1];
+      line_arr.push(input);
+    });
+    board.push(line_arr);
+    if (board.length >= N)
+      rl.close();
   }
 })
-rl.on('close', () => {
 
+function transformFromMinusToPlus(num) {
+  let div = Math.trunc(num / -N);
+
+  div += 1;
+  num += (div * N);
+  return num;
+}
+
+function calcurateMove(count, command, r, c) {
+  switch (command) {
+    case 'U':
+      r -= count;
+      if (r < 0)
+        r = transformFromMinusToPlus(r);
+      break;
+    case 'D':
+      r += count;
+      r %= N;
+      break;
+    case 'R':
+      c += count;
+      c %= N;
+      break;
+    case 'L':
+      c -= count;
+      if (c < 0)
+        c = transformFromMinusToPlus(c);
+      break;
+  }
+  // console.log(r, c);
+  return [r, c];
+}
+
+function goormiGame() {
+  let r = goormi.r - 1;
+  let c = goormi.c - 1;
+  let ret = 0;
+
+  while (1) {
+    let obj = board[r][c];
+
+    // console.log(obj);
+    ret++;
+    obj.goormi = true;
+    [r, c] = calcurateMove(obj.count, obj.command, r, c);
+    if (board[r][c].goormi === true)
+      break;
+  }
+  return ret;
+}
+
+function playerGame() {
+  let r = player.r - 1;
+  let c = player.c - 1;
+  let ret = 0;
+
+  while (1) {
+    let obj = board[r][c];
+
+    // console.log(obj);
+    ret++;
+    obj.player = true;
+    [r, c] = calcurateMove(obj.count, obj.command, r, c);
+    if (board[r][c].player === true)
+      break;
+  }
+  return ret;
+}
+
+rl.on('close', () => {
+  let goormiCnt;;
+  let playerCnt;
+  
+  goormiCnt = goormiGame();
+  playerCnt = playerGame();
+  if (playerCnt > goormiCnt) {
+    console.log('player', playerCnt);
+  }
+  else {
+    console.log('goorm', goormiCnt);
+  }
+  console.log(board);
 })
 
